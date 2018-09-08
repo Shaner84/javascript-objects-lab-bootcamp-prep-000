98c695ed96697f320e9ae5f7f7e9fd55c1195485
sreddy3@cox.net Reddy <sreddy3@cox.net>
2018-09-08 01:26:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,4 +12,8 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
 
 function deleteFromObjectByKey(object, key){
   const = newObject = Object.assign({}, object)
+  
+  delete newObject[key]
+  
+  return newObject
 }
